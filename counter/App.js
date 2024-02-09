9a2b9792635b497f128313e9d29f800319c14309
Sumit Chauhan <chauhansumit9009@gmail.com>
2024-02-10 00:03:02 +0530

    .

--- counter/App.js
+++ counter/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Strange things happen, sometimes it is intended, sometimes unintended.</Text>
+      <Text>The best part is, apart from keeping on eye on every other variable, relax and let things happen on its own.</Text>
+      <Text>Usually I dont give advices, but sometimes I do, mostly depends on the intellect of the person I am talking to.</Text>
       <StatusBar style="auto" />
     </View>
   );
